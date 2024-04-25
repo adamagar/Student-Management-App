ab1ebf5647216944430ea5635d49ef714706673b
Adam Agar <adamagar88@gmail.com>
2024-04-24 21:54:40 -0400

    Create base structure of the project

--- src/main/java/com/project/contactmessage/controller/ContactMessageController.java
+++ src/main/java/com/project/contactmessage/controller/ContactMessageController.java
@@ -1,14 +1,12 @@
 package com.project.contactmessage.controller;
 
-import com.project.contactmessage.entity.ContactMessage;
+import com.project.contactmessage.dto.ContactMessageRequest;
+import com.project.contactmessage.dto.ContactMessageResponse;
 import com.project.contactmessage.service.ContactMessageService;
 import com.project.payload.response.ResponseMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -22,6 +20,16 @@ public class ContactMessageController {
     @PostMapping("/save")
     public ResponseMessage<ContactMessageResponse> save(@RequestBody @Valid ContactMessageRequest contactMessageRequest)
     {
+        return contactMessageRequest.save(contactMessageRequest);
+    }
 
+    @GetMapping("/getAll")
+    public Page<ContactMessageResponse> getAll(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "sort", defaultValue = "dateTime") String sort,
+            @RequestParam(value = "type", defaultValue = "desc") String type
+    ){
+        return contactMessageService.getAll(page, size, sort, type);
     }
 }
